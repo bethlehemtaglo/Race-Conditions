5f6ddcfe82479c6a85f770a52a52a7821be787c2
bethlehemtaglo <bethlehemtaglo@gmail.com>
2025-01-11 14:41:33 +0100

    task 4 with racing conditions removed

--- foo.c
+++ foo.c
@@ -5,13 +5,16 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t mutex;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
     for (int j = 0; j < 1000000; j++) {
+        pthread_mutex_lock(&mutex);
         i++;
-        }
+        pthread_mutex_unlock(&mutex);
+    }
     printf("this is i incremented: %d\n", i);
     return NULL;
 }
@@ -19,7 +22,9 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
     for (int j = 0; j < 1000000; j++) {
+        pthread_mutex_lock(&mutex);
         i--;
+        pthread_mutex_unlock(&mutex);
         }
     printf("this is i decremented: %d\n", i);
     return NULL;
@@ -32,6 +37,7 @@ int main(){
     // Hint: search the web! Maybe try "pthread_create example"?
     
     pthread_t thread_1, thread_2;
+    pthread_mutex_init(&mutex, NULL);
 
     if (pthread_create(&thread_1, NULL, incrementingThreadFunction, NULL) != 0){
         fprintf(stderr,"error has happended with incrementing \n");
@@ -49,8 +55,10 @@ int main(){
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);  
-    
+
     printf("The magic number is: %d\n", i);
+
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
 
