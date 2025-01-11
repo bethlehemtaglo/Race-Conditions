aa55e3f351f4543a9edb74faa193295ead731ef2
bethlehemtaglo <bethlehemtaglo@gmail.com>
2025-01-11 14:34:51 +0100

    task 3 in c

--- foo.c
+++ foo.c
@@ -9,11 +9,19 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for (int j = 0; j < 1000000; j++) {
+        i++;
+        }
+    printf("this is i incremented: %d\n", i);
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int j = 0; j < 1000000; j++) {
+        i--;
+        }
+    printf("this is i decremented: %d\n", i);
     return NULL;
 }
 
@@ -23,10 +31,26 @@ int main(){
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
     
+    pthread_t thread_1, thread_2;
+
+    if (pthread_create(&thread_1, NULL, incrementingThreadFunction, NULL) != 0){
+        fprintf(stderr,"error has happended with incrementing \n");
+        return 1;
+    }
+
+    if (pthread_create(&thread_2, NULL, decrementingThreadFunction, NULL) != 0) {
+        fprintf(stderr, "Error creating thread2\n");
+        return 1;
+    }
+
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+    // Hint: Use `pthread_join`  
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);  
     
     printf("The magic number is: %d\n", i);
     return 0;
 }
+
